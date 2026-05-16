c73082734fd4f02a80c8b7b8522a7d9e1c183c04
Marcossb125 <marcossbarja@gmail.com>
2026-05-16 23:14:29 +0200

    up

--- Convergence/src/environments/environment.development.ts
+++ Convergence/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  backendUrl: 'http://localhost:3000'
+  backendUrl: 'http://74.242.219.101:3000'
 };
--- Convergence/src/environments/environment.ts
+++ Convergence/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://localhost:3000'
+  backendUrl: 'http://74.242.219.101:3000'
 };
